d6a6e228cdd0614fbcc044e90d8404865b81cf3b
Bruno Sastre <bsastre@genexus.com>
2024-06-20 04:29:41 -0300

    Update copy-tasks.js
    copy css folder and fonts to the showcase as well so, It can be shared independetlly of /dist

--- packages/mercury/copy-tasks.js
+++ packages/mercury/copy-tasks.js
@@ -9,21 +9,29 @@ Step 1: Copy 'src/assets/' to 'dist/assets/', except from '_generated' directory
 
 Step 2: Copy 'src/assets/icons/_generated/*' to 'dist/assets/icons/'
 
-Step 3: Copy the icons to 'showcase/icons/' as well so that the showcase folder can
-be distributed with its own icons, independently of 'dist/assets/icons'
+Step 3: Copy generated icons to 'showcase/assets/icons/' as well so that the showcase folder
+can be distributed with its own icons, independently of 'dist/assets/icons'
+
+Step 4: Copy "dist/css/" to 'showcase/css/' as well so that the showcase folder
+can be distributed with its own css/, independently of 'dist/css/'
+
+Step 5: Copy "dist/assets/fonts/" to 'showcase/assets/fonts/' as well so that the showcase folder
+can be distributed with its own fonts/, independently of 'dist/assets/fonts/'
 */
 
 const SVG_SASS_GENERATOR_GENERATED_FOLDER = "_generated";
 const ASSETS_SRC = "./src/assets/";
 const ASSETS_DIST = "./dist/assets/";
+const ASSETS_CSS = "./dist/css/";
+const ASSETS_FONTS = "./dist/assets/fonts/";
 const GENERATED_ICONS_PATH = path.join(
   ASSETS_SRC,
   `icons/${SVG_SASS_GENERATOR_GENERATED_FOLDER}`
 );
 const DIST_ICONS_PATH = path.join(ASSETS_DIST, "icons");
-const SHOWCASE_PATH = "./showcase";
+const SHOWCASE_PATH = "showcase/";
 
-// Copy Assets
+// Copy Folders
 const copyFolderSync = (source, target) => {
   if (!fs.existsSync(target)) {
     fs.mkdirSync(target, { recursive: true });
@@ -52,12 +60,20 @@ const copyFolderSync = (source, target) => {
 
 // Step 1:
 copyFolderSync(ASSETS_SRC, ASSETS_DIST);
+
 // Step 2
 copyFolderSync(GENERATED_ICONS_PATH, DIST_ICONS_PATH);
+
 // Step 3
 copyFolderSync(
   GENERATED_ICONS_PATH,
   path.join(SHOWCASE_PATH, "assets", "icons")
 );
+
+// Step 4
+copyFolderSync(ASSETS_CSS, path.join(SHOWCASE_PATH, "css/"));
+
+// Step 5
+copyFolderSync(ASSETS_FONTS, path.join(SHOWCASE_PATH, "assets", "fonts"));
 
 console.log("copy-task.js copied successfully");
