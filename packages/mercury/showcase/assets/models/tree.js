b8c0908a3a074876c166bc07ae9baaf41ba11b3c
Bruno Sastre <bsastre@genexus.com>
2024-07-11 01:29:02 -0300

    fix "Root Module" node `startImgSrc`

--- packages/mercury/showcase/assets/models/tree.js
+++ packages/mercury/showcase/assets/models/tree.js
@@ -182,7 +182,7 @@ export const iconsModel = [
       {
         id: "Root_Module",
         caption: "Root Module",
-        startImgSrc: "item--module",
+        startImgSrc: "objects/module",
         expanded: true,
         items: [
           {
