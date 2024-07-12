483e80cc6e00aff0497a789e220641d457ff7831
Nicolás Cámera <ncamera@genexus.com>
2024-07-12 14:40:27 -0300

    Update types in bundles.ts

--- packages/mercury/src/bundles/bundles.ts
+++ packages/mercury/src/bundles/bundles.ts
@@ -1,15 +1,28 @@
 export type MercuryBundles = MercuryBundle[];
 
-export type MercuryBundle = MercuryBundleBase | MercuryBundleComponent;
+export type MercuryBundle =
+  | MercuryBundleBase
+  | MercuryBundleComponent
+  | MercuryBundleReset
+  | MercuryBundleUtil;
 
-export type MercuryBundleBase = "base" | "icons";
+export type MercuryBundleBase = "base/base" | "base/icons";
 
 export type MercuryBundleComponent =
-  | "checkbox"
-  | "code"
-  | "combo-box"
-  | "dialog"
-  | "radio-group"
-  | "tab"
-  | "tabular-grid"
-  | "tree-view";
+  | "components/button"
+  | "components/checkbox"
+  | "components/code"
+  | "components/combo-box"
+  | "components/dialog"
+  | "components/edit"
+  | "components/radio-group"
+  | "components/tab"
+  | "components/tabular-grid"
+  | "components/tree-view";
+
+export type MercuryBundleReset = "resets/box-sizing";
+
+export type MercuryBundleUtil =
+  | "utils/form"
+  | "utils/layout"
+  | "utils/typography";
