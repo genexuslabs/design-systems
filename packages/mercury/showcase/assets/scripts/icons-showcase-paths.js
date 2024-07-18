baa9eaf1b75032fbbe49d4184953d8f2c02e2f30
Bruno Sastre <54677414+bsastregx@users.noreply.github.com>
2024-07-18 10:35:01 -0300

    [mercury] copy assets manager to showcase, update svg-sass-generator, and add ch-image styles and tokens (#199)
    * Add `MERCURY_ASSETS.js` to `copy-tasks.js`
    
    * Manual copy of `assets-manager.js` to `showcase` directory
    
    * move `navbar-items.json` to a new json folder
    
    * Move showcase scripts to a `/scripts` folder under `/assets`
    
    * Remove `allObjectsIcons` tree model from tree model
    
    Not needed anymore with the new icons implementation
    
    * Move showcase stylesheets to a `/styles` folder under `/assets`
    
    * update `$icons-path` so that it works with the showcase
    
    * update showcase `scripts.js` file for every html
    
    * remove "all objects icons" from tree showcase.
    
    Just display icons in the second case "with icons".
    
    * fix "Root Module" node `startImgSrc`
    
    * update icon showcase
    
    update from svg-sass-generator showcase, to mercury custom icons showcase
    
    * remove testing showcase html
    
    * icons classes clean up
    
    remove the old way of defining icons classes and mixins, in favor of the new way by using the `assets-manager`
    
    * add styles for ch-image (icon)
    
    * add tokens for icon size (not the box)
    
    * add back `icon-background-common-styles` mixin
    
    * add icon html showcase
    
    * begin to add ch-iamge on button (not finished)
    
    * limit comments to 80 characters per line.
    
    * update updated svg-sass-generator to `1.1.18`
    
    * Delete _form-input.scss
    
    form-input element will be replaced by `ch-edit`
    
    * Update mercury.scss
    
    - remove @import "./icons/form-input" (it ill be replaced by `ch.edit`)
    - add sass parameter for $icons-placeholders
    - remove `$ignored-multicolor-categories` (not needed anymore)
    - remove `$ignored-monochrome-categories` (not needed anymore)
    
    * Update _tabular-grid-styles.scss
    
    - add "__placeholders" suffix to icons categories placeholders imports, since the sass filenames generated by `svg-sass-generator` have been updated.
    
    * Update main.css
    
    Fix copy image path
    
    * remove `mer` prefix from icons variables
    
    * Update package.json
    
    - remove `mer` from `vendorPrefix`
    - remove `&& npm run showcase.scss` from `build` and `showcase.scss` scripts
    
    * ignore notes.txt
    
    * Add new files, and tidy up the copy-tasks.js file
    
    Add the following files to the showecase copy process :
    - Step 6: Copy 'dist/assets-manager.js' (destination updated)
    - Step 7: Copy 'dist/bundles.js'
    - Step 8: Copy 'dist/assets/MERCURY_ASSETS.js'
    
    * update body `sidebar` not valid attribute to valid `data-sidebar` attribute
    
    * Delete tree-icons.scss
    
    Not required anymore with the new assets-manager
    
    * rename `$form-controls-classes` to the singlar form `$form-control-classes`
    
    This is to adhere to the rest of variables, that are in the singular form.
    
    * Update mercury.scss
    
    - general re-order by alphabetical criteria
    - add $image for ch-image
    - remove `$icons-elements`in favor of `$icon-classes`
    - update `mercury-only` mixin, by copying and pasting the `mercury` mixin signature, and setting all values to false
    - add `$icons-placeholders`
    
    * update unsupported `sidebar` attribute for `data-sidebar` and use default mercury `getImagePathCallback` for ch-image

---
+++ packages/mercury/showcase/assets/scripts/icons-showcase-paths.js
@@ -0,0 +1,9 @@
+import { getIconPath } from "./assets-manager.js";
+
+export const BUTTON_ICONS = {
+  GEMINI_TOOLS_ADD_CIRCLE_ON_PRIMARY: getIconPath({
+    category: "gemini-tools",
+    name: "add-circle",
+    colorType: "on-primary"
+  })
+};
