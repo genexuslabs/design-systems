48a793df4be93c722ea3e0151cb56c2d00ed17ed
Bruno Sastre <bsastre@genexus.com>
2024-07-23 14:18:22 -0300

    revert commit `309e238`

--- packages/svg-sass-generator/ts/processor-sass.ts
+++ packages/svg-sass-generator/ts/processor-sass.ts
@@ -71,12 +71,9 @@ export function processIconsSass(sourceDir: string, iconsArray: string[]) {
     multicolor: {},
   };
   processIconsCatalog(iconsCatalog, categoriesListsObject);
+  saveIconsListsOnDisk(categoriesListsObject);
   saveIconsCategoriesImportsOnDisk("variables");
-  // The following functions were commented because at the time of writing it only makes sense
-  // to generate the custom properties. If we need to generate placeholders or categories lists
-  // in the future, these functions are here ready to be applied.
-  // saveIconsListsOnDisk(categoriesListsObject);
-  // saveIconsCategoriesImportsOnDisk("placeholders");
+  saveIconsCategoriesImportsOnDisk("placeholders");
   // saveMixinsOnDisk();
 }
 
@@ -173,12 +170,11 @@ const processCatalogCategory = (
     iconType,
     `${categoryName}${CATEGORY_VARIABLES_FILENAME_SUFFIX}`
   );
-  // At the time of writing we no longer want to generate the placeholders selectors.
-  // saveSassOnDisk(
-  //   sassFilesContent.placeHoldersSelectorsSass,
-  //   iconType,
-  //   `${categoryName}${CATEGORY_PLACEHOLDERS_FILENAME_SUFFIX}`
-  // );
+  saveSassOnDisk(
+    sassFilesContent.placeHoldersSelectorsSass,
+    iconType,
+    `${categoryName}${CATEGORY_PLACEHOLDERS_FILENAME_SUFFIX}`
+  );
 };
 
 const createSassFiles = (
