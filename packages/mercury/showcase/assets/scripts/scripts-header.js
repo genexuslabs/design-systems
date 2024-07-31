dab1d289d931e8936b7194508eabde6fadd81c43
Bruno Sastre <bsastre@genexus.com>
2024-07-31 17:42:33 -0300

    update chameleon to 6.0.0-next.22

--- packages/mercury/showcase/assets/scripts/scripts-header.js
+++ packages/mercury/showcase/assets/scripts/scripts-header.js
@@ -1,8 +1,8 @@
 import { BUTTON_ICONS } from "./icons-showcase-paths.js";
 // All definitions in this script are used for local testing purposes
 // only. The IDE Web's SDK implements all this and more
-import { defineCustomElements } from "https://unpkg.com/@genexus/chameleon-controls-library@6.0.0-next.19/dist/esm/loader.js";
-import { registryProperty } from "https://unpkg.com/@genexus/chameleon-controls-library@6.0.0-next.19/dist/esm/index.js";
+import { defineCustomElements } from "https://unpkg.com/@genexus/chameleon-controls-library@6.0.0-next.22/dist/esm/loader.js";
+import { registryProperty } from "https://unpkg.com/@genexus/chameleon-controls-library@6.0.0-next.22/dist/esm/index.js";
 import { getImagePathCallbackDefinitions } from "./assets-manager.js";
 
 // Register getImagePath callbacks
