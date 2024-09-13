0e43b0d10111820914c6a8f5e541e3e006b6db2c
Bruno Sastre <54677414+bsastregx@users.noreply.github.com>
2024-09-13 11:11:45 -0300

    remove "tooltip" from "form-full" bundle

--- packages/mercury/src/bundles.ts
+++ packages/mercury/src/bundles.ts
@@ -31,7 +31,8 @@ export type MercuryBundleComponent =
   | "components/tab"
   | "components/tabular-grid"
   | "components/ticket-list"
-  | "components/tree-view";
+  | "components/tree-view"
+  | "components/tooltip";
 
 export type MercuryBundleComponentForm =
   | "components/button"
@@ -40,8 +41,7 @@ export type MercuryBundleComponentForm =
   | "components/edit"
   | "components/icon"
   | "components/radio-group"
-  | "components/slider"
-  | "components/tooltip";
+  | "components/slider";
 
 export type MercuryBundleReset = "resets/box-sizing";
 
