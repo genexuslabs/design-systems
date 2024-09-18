b150bcb3d4c8ac16090ca9855c91094fea0aa824
Bruno Sastre <54677414+bsastregx@users.noreply.github.com>
2024-09-18 15:11:56 -0300

    `[mercury]` add accordion showcase page (#340)
    * add accordion to showcase
    
    * add accordion semantic tokens and disabled states
    
    * fix writting on form showcase pasge
    
    * add "accordion" to the "doc-spacing" showcase page.
    
    * explain "spacing classes" on the accordion showcase page
    
    * update showcase generated files

---
+++ packages/mercury/showcase/assets/models/accordion.js
@@ -0,0 +1,43 @@
+export const accordionModel = [
+  {
+    id: "file",
+    caption: "objects: file",
+    expanded: true
+  },
+  {
+    id: "domain",
+    caption: "objects: domain"
+  },
+  {
+    id: "procedure",
+    caption: "objects: procedure"
+  },
+  {
+    id: "document",
+    caption: "objects: document"
+  }
+];
+
+export const accordionWithIconsModel = [
+  {
+    id: "file",
+    caption: "objects: file",
+    startImgSrc: "objects/file",
+    expanded: true
+  },
+  {
+    id: "domain",
+    caption: "objects: domain",
+    startImgSrc: "objects/domain"
+  },
+  {
+    id: "procedure",
+    caption: "objects: procedure",
+    startImgSrc: "objects/procedure"
+  },
+  {
+    id: "document",
+    caption: "objects: document",
+    startImgSrc: "objects/document"
+  }
+];
