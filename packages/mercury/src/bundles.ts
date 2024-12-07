c0396c3bfd52fabdb01d02b5feff41c33b84bdf0
Nicolás Cámera <81186472+ncamera@users.noreply.github.com>
2024-12-07 14:42:01 -0300

    Add support to style the toggle component (#403)

--- packages/mercury/src/bundles.ts
+++ packages/mercury/src/bundles.ts
@@ -43,7 +43,8 @@ export type MercuryBundleComponentForm =
   | "components/edit"
   | "components/icon"
   | "components/radio-group"
-  | "components/slider";
+  | "components/slider"
+  | "components/toggle";
 
 export type MercuryBundleReset = "resets/box-sizing";
 
@@ -121,6 +122,7 @@ export const getThemeBundles = (basePath: string) =>
     getThemeModelItem(basePath, "components/tab"),
     getThemeModelItem(basePath, "components/tabular-grid"),
     getThemeModelItem(basePath, "components/ticket-list"),
+    getThemeModelItem(basePath, "components/toggle"),
     getThemeModelItem(basePath, "components/tooltip"),
     getThemeModelItem(basePath, "components/tree-view"),
     getThemeModelItem(basePath, "components/widget"),
