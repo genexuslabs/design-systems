972bc1d9ed6042e827588026129dde17be6d3055
Nicolás Cámera <ncamera@genexus.com>
2025-01-05 16:42:53 -0300

    Update compatibility tables

--- packages/showcase/src/app/chameleon-compatibility/unanimo-compatibility-table.ts
+++ packages/showcase/src/app/chameleon-compatibility/unanimo-compatibility-table.ts
@@ -1,4 +1,7 @@
 export const unanimoCompatibilityTable: {
   version: string;
   chameleon: string;
-}[] = [{ version: "~0.23.0", chameleon: ">= 6.0.0-next.52" }];
+}[] = [
+  { version: "^0.24.0", chameleon: ">= 6.0.0-next.54" },
+  { version: "~0.23.0", chameleon: "6.0.0-next.52 - 6.0.0-next.53" }
+];
