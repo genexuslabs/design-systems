a3e42e627e819bc816c248fe86097ac392e79c67
Nicolás Cámera <ncamera@genexus.com>
2025-01-08 03:01:36 -0300

    Reduce flickering when navigating in the app

--- packages/showcase/src/user-controls/runtime-bundles/runtime-bundles.component.ts
+++ packages/showcase/src/user-controls/runtime-bundles/runtime-bundles.component.ts
@@ -2,8 +2,11 @@ import {
   Component,
   computed,
   CUSTOM_ELEMENTS_SCHEMA,
+  effect,
+  HostBinding,
   inject,
   input,
+  signal,
   ViewEncapsulation
 } from "@angular/core";
 
@@ -27,8 +30,14 @@ import {
   encapsulation: ViewEncapsulation.None
 })
 export class RuntimeBundlesComponent {
+  // Useful to reduce flickering when loading bundles
+  @HostBinding("class.data-bundle-loaded") className = false;
+
   dsService = inject(DesignSystemService);
+
+  avoidFlashOfUnstyledContent = input(false);
   bundles = input<MercuryBundleOptimized[] | UnanimoBundleOptimized[]>([]);
+
   mercuryThemeModel = computed<ThemeModel>(() =>
     getBundlesMercury(this.bundles(), MERCURY_BASE_CSS_URL, "mercury/")
   );
@@ -36,4 +45,16 @@ export class RuntimeBundlesComponent {
   unanimoThemeModel = computed<ThemeModel>(() =>
     getBundlesUnanimo(this.bundles(), UNANIMO_BASE_CSS_URL, "unanimo/")
   );
+
+  mercuryThemeLoaded = signal(false);
+  unanimoThemeLoaded = signal(false);
+
+  constructor() {
+    effect(() => {
+      this.className = this.mercuryThemeLoaded() && this.unanimoThemeLoaded();
+    });
+  }
+
+  handleMercuryThemeLoaded = () => this.mercuryThemeLoaded.set(true);
+  handleUnanimoThemeLoaded = () => this.unanimoThemeLoaded.set(true);
 }
