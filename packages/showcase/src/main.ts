e5080ff5144560e36d9349f21ca347a2298284f5
Nicolás Cámera <ncamera@genexus.com>
2025-01-15 16:44:29 -0300

    Use Mercury and Unanimo from npm

--- packages/showcase/src/main.ts
+++ packages/showcase/src/main.ts
@@ -2,8 +2,11 @@ import { bootstrapApplication } from "@angular/platform-browser";
 import { appConfig } from "./app/app.config";
 import { AppComponent } from "./app/app.component";
 import { defineCustomElements } from "@genexus/chameleon-controls-library/loader";
+import { setBundleMapping } from "@genexus/mercury/bundles.js";
 import { registerMercury } from "@genexus/mercury/register-mercury.js";
+import { bundleToHashMappings } from "../.mercury/bundle-to-hash-mappings";
 
+setBundleMapping(bundleToHashMappings);
 registerMercury();
 defineCustomElements(window);
 
