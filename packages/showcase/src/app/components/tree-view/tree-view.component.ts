b0c1794fb4ab0416b22f2f268316be87c1d14058
Bruno Sastre <bsastre@genexus.com>
2025-01-18 17:18:59 +0100

    `[mercury][showcase][tree-view]` Remove usused icon

--- packages/showcase/src/app/components/tree-view/tree-view.component.ts
+++ packages/showcase/src/app/components/tree-view/tree-view.component.ts
@@ -33,12 +33,6 @@ import { iconsModel, noIconsModel } from "./models";
 export class TreeViewComponent {
   router = inject(Router);
 
-  ADD_CIRCLE_ICON = getIconPath({
-    category: "gemini-tools",
-    name: "add-circle",
-    colorType: "on-elevation"
-  });
-
   hiddenTreeViews = input<string>("");
 
   /**
