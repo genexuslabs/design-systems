4e57571797315161b7d932b427d6046331ad3a6a
Bruno Sastre <bruno.sastre@globant.com>
2025-01-24 04:04:24 +0100

    `[shocase][tabular-grid]` Include remaining shells

--- packages/showcase/src/app/components/tabular-grid/metadata.ts
+++ packages/showcase/src/app/components/tabular-grid/metadata.ts
@@ -48,6 +48,158 @@ export const tabularGridMetadata = {
           }
         ]
       }
+    },
+    rowSet: {
+      linkId: "row-set",
+      title: "2. Row set",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    selectionSimpleKeyboardFocus: {
+      linkId: "selection-simple-keyboard-focus",
+      title: "3. Selection: simple + keyboard: focus",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    selectionSimpleKeyboardSelect: {
+      linkId: "selection-simple-keyboard-select",
+      title: "4. Selection: simple + keyboard: select",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    selectionMultipleKeyboardFocus: {
+      linkId: "selection-multiple-keyboard-focus",
+      title: "5. Selection: multiple + keyboard: focus",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    selectionMultipleKeyboardSelect: {
+      linkId: "selection-multiple-keyboard-select",
+      title: "6. Selection: multiple + keyboard: select",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    rowMarkKeyboardSelect: {
+      linkId: "row-mark-keyboard-select",
+      title: "7. Row mark + keyboard: select",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    rowMarkKeyboardFocus: {
+      linkId: "row-mark-keyboard-focus",
+      title: "8. Row mark + keyboard: focus",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    rowReorderDragging: {
+      linkId: "row-reorder-dragging",
+      title: "9. Row reorder (dragging)",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    rowAction: {
+      linkId: "row-action",
+      title: "10. Row action",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    treeGrid: {
+      linkId: "tree-grid",
+      title: "11. Tree grid",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    propertyGrid: {
+      linkId: "property-grid",
+      title: "12. Property grid",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    alignCellsBlockStart: {
+      linkId: "align-cells-block-start",
+      title: "13. Align cells: block start",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    alignCellsBlockCenter: {
+      linkId: "align-cells-block-center",
+      title: "14. Align cells: block center",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    alignCellsBlockEnd: {
+      linkId: "align-cells-block-end",
+      title: "15. Align cells: block end",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    alignCellsInlineStart: {
+      linkId: "align-cells-inline-start",
+      title: "16. Align cells: inline start",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    alignCellsInlineCenter: {
+      linkId: "align-cells-inline-center",
+      title: "17. Align cells: inline center",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
+    },
+
+    alignCellsInlineEnd: {
+      linkId: "align-cells-inline-end",
+      title: "18. Align cells: inline end",
+      template: {
+        tag: "ch-tabular-grid",
+        class: "tabular-grid"
+      }
     }
   }
 } as const satisfies ComponentMetadata;
