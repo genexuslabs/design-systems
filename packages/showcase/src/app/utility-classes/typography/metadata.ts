5a1ecf4cfeec0587d0a1ffb2066668c9a867a919
Bruno Sastre <bsastre@genexus.com>
2025-02-05 14:17:20 +0100

    `[mercury][showcase][typography]` Add body regular

--- packages/showcase/src/app/utility-classes/typography/metadata.ts
+++ packages/showcase/src/app/utility-classes/typography/metadata.ts
@@ -115,6 +115,38 @@ export const typographyMetadata = {
       template: [
         { tag: "p", class: "subtitle-regular-xs", children: SUBTITLE_CONTENT }
       ]
+    },
+
+    // Body Regular
+
+    bodyRegularXL: {
+      linkId: "body-regular-xl",
+      title: "3.1. Body Regular XL",
+      template: [{ tag: "p", class: "body-regular-xl", children: BODY_CONTENT }]
+    },
+
+    bodyRegularL: {
+      linkId: "body-regular-l",
+      title: "3.2. Body Regular L",
+      template: [{ tag: "p", class: "body-regular-l", children: BODY_CONTENT }]
+    },
+
+    bodyRegularM: {
+      linkId: "body-regular-m",
+      title: "3.3. Body Regular M",
+      template: [{ tag: "p", class: "body-regular-m", children: BODY_CONTENT }]
+    },
+
+    bodyRegularS: {
+      linkId: "body-regular-s",
+      title: "3.4. Body Regular S",
+      template: [{ tag: "p", class: "body-regular-s", children: BODY_CONTENT }]
+    },
+
+    bodyRegularXS: {
+      linkId: "body-regular-xs",
+      title: "3.5. Body Regular XS",
+      template: [{ tag: "p", class: "body-regular-xs", children: BODY_CONTENT }]
     }
   }
 } as const satisfies ComponentMetadata;
