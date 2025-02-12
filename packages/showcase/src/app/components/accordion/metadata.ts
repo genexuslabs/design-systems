4fc5ea162c07d7ba31406644abbe50ccde0f6396
Bruno Sastre <bsastre@genexus.com>
2025-02-12 03:22:25 +0100

    `[showcase][accordion]` Include "filled: disabled" case

--- packages/showcase/src/app/components/accordion/metadata.ts
+++ packages/showcase/src/app/components/accordion/metadata.ts
@@ -13,24 +13,27 @@ export const accordionMetadata = {
   title: "Accordion",
   description:
     "An accordion is a UI component that organizes content into collapsible sections, allowing users to expand or collapse panels as needed. It helps keep interfaces clean by displaying only relevant information while providing quick access to additional details.",
-  accordionItemFile: {
-    slot: "file",
-    description: `The File object in GeneXus is designed to handle file-related operations within an application. It allows developers to work with files stored locally or on a server by providing methods to upload, download, open, read, write, and manipulate files in various formats. This object is particularly useful for managing documents, images, or any other type of file interaction, making it easier to handle file-based processes within business applications.`
+  common: {
+    itemFile: {
+      slot: "file",
+      description: `The File object in GeneXus is designed to handle file-related operations within an application. It allows developers to work with files stored locally or on a server by providing methods to upload, download, open, read, write, and manipulate files in various formats. This object is particularly useful for managing documents, images, or any other type of file interaction, making it easier to handle file-based processes within business applications.`
+    },
+    itemDomain: {
+      slot: "domain",
+      description: `The Domain object in GeneXus allows developers to define a specific data type with associated validation rules, formatting, and constraints, which can be reused across various parts of an application. It helps ensure consistency and standardization for commonly used data types, such as emails, phone numbers, or currencies. By creating Domains, developers can streamline application maintenance and enforce uniform data validation throughout the system.`
+    },
+    itemProcedure: {
+      slot: "procedure",
+      description: `The Procedure object in GeneXus is used to define a sequence of actions or logic that can be executed within an application. It allows developers to create reusable routines to process data, perform calculations, update databases, or call external services. Procedures can be triggered by user actions or scheduled tasks, and they offer flexibility for handling complex business logic while improving the modularity and maintainability of the application.`
+    },
+    itemDocument: {
+      slot: "document",
+      description: `The Document object in GeneXus is designed to handle text-based content, such as generating, manipulating, and exporting documents in various formats like PDF, Word, or HTML. It allows developers to create dynamic reports, invoices, or other formal documents by merging static text with data from the application. The Document object is ideal for automating the creation of printable and shareable files within business workflows.`
+    }
   },
-  accordionItemDomain: {
-    slot: "domain",
-    description: `The Domain object in GeneXus allows developers to define a specific data type with associated validation rules, formatting, and constraints, which can be reused across various parts of an application. It helps ensure consistency and standardization for commonly used data types, such as emails, phone numbers, or currencies. By creating Domains, developers can streamline application maintenance and enforce uniform data validation throughout the system.`
-  },
-  accordionItemProcedure: {
-    slot: "procedure",
-    description: `The Procedure object in GeneXus is used to define a sequence of actions or logic that can be executed within an application. It allows developers to create reusable routines to process data, perform calculations, update databases, or call external services. Procedures can be triggered by user actions or scheduled tasks, and they offer flexibility for handling complex business logic while improving the modularity and maintainability of the application.`
-  },
-  accordionItemDocument: {
-    slot: "document",
-    description: `The Document object in GeneXus is designed to handle text-based content, such as generating, manipulating, and exporting documents in various formats like PDF, Word, or HTML. It allows developers to create dynamic reports, invoices, or other formal documents by merging static text with data from the application. The Document object is ideal for automating the creation of printable and shareable files within business workflows.`
-  },
+
   codeSnippets: {
-    // accordion fiiled
+    // accordion filled
     filled: {
       linkId: "accordion-filled",
       title: "Filled",
@@ -48,20 +51,27 @@ export const accordionMetadata = {
         properties: [{ name: "model", value: "genexusObjects", state: true }],
         children: accordionCommonChildren
       }
-    }
+    },
 
-    // filledDisabled: {
-    //   linkId: "accordion-filled-disabled",
-    //   title: "Filled: Disabled",
-    //   template: {
-    //     tag: "ch-accordion-render",
-    //     class: "accordion-filled elevation-1",
-    //     properties: [
-    //       { name: "model", value: "...", variable: true },
-    //       { name: "showLines", value: "all" }
-    //     ],
-    //     children: accordionCommonChildren
-    //   }
-    // }
+    filledDisabled: {
+      linkId: "accordion-filled-disabled",
+      title: "Filled: Disabled",
+      states: [
+        {
+          name: "genexusObjects",
+          type: "AccordionModel",
+          value: accordionModel
+        }
+      ],
+      imports: [chameleonImportType("AccordionModel")],
+      template: {
+        tag: CH_TAG,
+        properties: [
+          { name: "model", value: "genexusObjects", state: true },
+          { name: "disabled", value: true }
+        ],
+        children: accordionCommonChildren
+      }
+    }
   }
 };
