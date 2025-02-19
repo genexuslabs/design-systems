11ca3addb17a2763acb490d55bbb2b115a9a7c00
Nicolás Cámera <ncamera@genexus.com>
2025-02-19 16:22:05 -0300

    Update compatibility table

--- packages/showcase/src/app/chameleon-compatibility/mercury-compatibility-table.ts
+++ packages/showcase/src/app/chameleon-compatibility/mercury-compatibility-table.ts
@@ -2,7 +2,8 @@ export const mercuryCompatibilityTable: {
   version: string;
   chameleon: string;
 }[] = [
-  { version: "^0.15.0", chameleon: ">= 6.2.0" },
+  { version: "^0.16.0", chameleon: "^6.3.0" },
+  { version: "0.15.0 - 0.15.1", chameleon: "6.2.0 - 6.2.1" },
   { version: "0.10.0 - 0.14.0", chameleon: "6.0.0-next.54 - 6.1.1" },
   { version: "~0.9.15", chameleon: "6.0.0-next.52 - 6.0.0-next.53" }
 ];
